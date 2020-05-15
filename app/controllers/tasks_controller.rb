f720009a02874e5c8e9f3431e4e43c7d02de5a63
(Jonathan Ng) <nzyjonathan@gmail.com>
2020-05-15 10:19:42 +0800

    update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,29 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @new = Task.new
+  end
+
+  def create
+    task = Task.new(task_params)
+    task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+    @edit_task = Task.find(params[:id])
+  end
+
+  def update
+    @update_task = Task.find(params[:id])
+    @update_task.update(task_params)
+    redirect_to task_path
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
